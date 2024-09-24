ee298df7e7e7e4eeb683b1f32eca080c826cfc9d
superiority <Agadjiev397@gmail.com>
2024-09-25 00:03:23 +0500

    array 2

--- index.php
+++ index.php
@@ -1,9 +1,41 @@
 <?php
 
 //--------------------------МАССИВЫ--------------------------//
+//
+//$arrays = [
+//    array(
+//        'first' => 'Anton',
+//        'second' => 'Micke',
+//        'third' => 'Jack',
+//    ),
+//    array(
+//        'first' => 'Ben',
+//        'second' => 'Slava',
+//        'third' => 'Tony',
+//    ),array(
+//        'first' => 1,
+//        'second' => 2,
+//        'third' => 3,
+//    ),
+//    array(
+//        'first' => 4,
+//        'second' => 5,
+//        'third' => 6,
+//    )
+//
+//];
+//
+//$second = array_column($arrays, 'second');
 
-$arr = [10, 'level', 'man'];
+//print_r($second);
 
-array_push($arr, 170);
+//-----------------------------------------
 
-var_dump($arr);
+$color = array ("a" => "red", "blue", "yellow");
+$color2 = array("b" => "red", "black", "blue");
+
+$result = array_diff($color, $color2);
+
+print_r($result);
+
+
