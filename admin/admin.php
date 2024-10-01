721dabd52d479c457f432a2a93ff6e40cbe51b3e
superiority <Agadjiev397@gmail.com>
2024-10-01 06:11:38 +0500

    work - admin

--- admin/admin.php
+++ admin/admin.php
@@ -1,22 +1,19 @@
-<?php require_once '../connect.php';
+<?php
+require_once '../connect.php';
+session_start();
 
 $login = $_POST['login'];
 $password = $_POST['password'];
 
-session_start();
-
-$sql = $pdo->prepare("SELECT id FROM user WHERE login=:login AND password=:password");
+$sql = $pdo->prepare("SELECT id, login FROM user WHERE login=:login AND password=:password");
 $sql->execute(array('login' => $login, 'password' => $password));
-$array=$sql->fetch(PDO::FETCH_ASSOC);
+$array = $sql->fetch(PDO::FETCH_ASSOC);
 
-if($array["id" > 0]) {
+if ($array && $array["id"] > 0) {
     $_SESSION['login'] = $array["login"];
     header('Location:/adminClose.php');
 } else {
     header('Location:/login.php');
 }
-
-//сегодня выходной, просто для коммита
-//сегодня выходной, просто для коммита
 
 
