ba900194048a95562273273ecf9d8dbfb18b58d4
superiority <Agadjiev397@gmail.com>
2024-10-07 21:38:28 +0500

    rename

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php require 'public/contact.php' ?>
 <?php require 'public/about.php' ?>
-<?php require 'public/uslugi.php' ?>
+<?php require 'public/services.php' ?>
 <?php require 'public/footer.php' ?>
