9a175420b3864fe245a593c5d7816de7e6a8838c
superiority <Agadjiev397@gmail.com>
2024-10-11 22:47:06 +0500

    refactor

--- admin/admin.php
+++ admin/admin.php
@@ -2,18 +2,29 @@
 require_once '../connect.php';
 session_start();
 
-$login = $_POST['login'];
-$password = $_POST['password'];
+// Проверка на пустые данные
+if (!empty($_POST['login']) && !empty($_POST['password'])) {
+    $login = trim($_POST['login']);
+    $password = trim($_POST['password']);
 
-$sql = $pdo->prepare("SELECT id, login FROM user WHERE login=:login AND password=:password");
-$sql->execute(array('login' => $login, 'password' => $password));
-$array = $sql->fetch(PDO::FETCH_ASSOC);
+    // Подготовленный SQL-запрос для защиты от SQL-инъекций
+    $sql = $pdo->prepare("SELECT id, login FROM user WHERE login = :login AND password = :password");
+    $sql->execute(['login' => $login, 'password' => $password]);
 
-if ($array && $array["id"] > 0) {
-    $_SESSION['login'] = $array["login"];
-    header('Location:/adminClose.php');
+    // Получаем данные пользователя
+    $user = $sql->fetch(PDO::FETCH_ASSOC);
+
+    // Если пользователь найден
+    if ($user) {
+        $_SESSION['login'] = $user['login'];
+        header('Location: /adminClose.php');
+        exit(); // Прерывание выполнения после редиректа
+    } else {
+        header('Location: /login.php');
+        exit(); // Прерывание выполнения после редиректа
+    }
 } else {
-    header('Location:/login.php');
+    // Возврат на страницу логина, если поля пустые
+    header('Location: /login.php');
+    exit();
 }
-
-
